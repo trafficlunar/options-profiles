a58b4c39aa2fb6a5a8e6abb52898de7e73920e05
Alfakynz <alfakynz.nyxaeris@gmail.com>
2026-06-19 17:29:24 +0200

    Compat for Sodium 0.8.12

--- common/src/main/java/net/trafficlunar/optionsprofiles/profiles/loaders/SodiumExtraLoader.java
+++ common/src/main/java/net/trafficlunar/optionsprofiles/profiles/loaders/SodiumExtraLoader.java
@@ -3,16 +3,13 @@ package net.trafficlunar.optionsprofiles.profiles.loaders;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import me.flashyreese.mods.sodiumextra.client.SodiumExtraClientMod;
-import me.flashyreese.mods.sodiumextra.client.gui.FogTypeConfig;
-import me.flashyreese.mods.sodiumextra.client.gui.SodiumExtraGameOptions;
+import me.flashyreese.mods.sodiumextra.client.config.SodiumExtraGameOptions;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.material.FogType;
 import net.trafficlunar.optionsprofiles.OptionsProfilesMod;
 
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.EnumMap;
 import java.util.Map;
 
 public class SodiumExtraLoader {
@@ -50,8 +47,8 @@ public class SodiumExtraLoader {
         SodiumExtraClientMod.options().detailSettings.biomeColors = configuration.detail_settings.biome_colors;
         SodiumExtraClientMod.options().detailSettings.skyColors = configuration.detail_settings.sky_colors;
 
-        SodiumExtraClientMod.options().renderSettings.globalFog = configuration.render_settings.global_fog;
-        SodiumExtraClientMod.options().renderSettings.fogTypeConfig = configuration.render_settings.fog_type_config;
+        SodiumExtraClientMod.options().renderSettings.multiDimensionFogControl = configuration.render_settings.global_fog;
+        SodiumExtraClientMod.options().renderSettings.fogStart = configuration.render_settings.fog_start;
         SodiumExtraClientMod.options().renderSettings.lightUpdates = configuration.render_settings.light_updates;
         SodiumExtraClientMod.options().renderSettings.itemFrame = configuration.render_settings.item_frame;
         SodiumExtraClientMod.options().renderSettings.armorStand = configuration.render_settings.armor_stand;
@@ -122,7 +119,7 @@ public class SodiumExtraLoader {
 
         public static class RenderSettings {
             public boolean global_fog;
-            public EnumMap<FogType, FogTypeConfig> fog_type_config;
+            public int fog_start;
             public boolean light_updates;
             public boolean item_frame;
             public boolean armor_stand;
